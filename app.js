be5030f38fbf6fe1c94a3d31fcee2da4a4d23ef6
adiuvo <conviva@smurfree.com>
2016-10-05 15:59:22 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+var electron_1 = require('electron');
+var main_1 = require('./app/main');
+main_1.default.main(electron_1.app, electron_1.BrowserWindow);
+//# sourceMappingURL=app.js.map
